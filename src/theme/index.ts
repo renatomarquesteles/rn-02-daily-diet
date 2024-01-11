aec0b28ab6162b3825f8b9df276314fae6c7a2d3
Renato <renatomarquesteles@gmail.com>
2024-01-11 19:02:37 -0300

    setup: fonts, theme and loading

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_DARK: '#639339,',
+    GREEN_MID: '#cbe4b4',
+    GREEN_LIGHT: '#e5f0db',
+
+    RED_DARK: '#bf3b44',
+    RED_MID: '#f3babd',
+    RED_LIGHT: '#f4e6e7',
+
+    GRAY_100: '#1b1d1e',
+    GRAY_200: '#333638',
+    GRAY_300: '#5c6265',
+    GRAY_400: '#b9bbbc',
+    GRAY_500: '#dddedf',
+    GRAY_600: '#eff0f0',
+    GRAY_700: '#fafafa',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold',
+  },
+  FONT_SIZE: {
+    XSM: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  },
+};
